e9b4ce25b998067df2e4b15ef8c598ef7a29264f
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-05-04 22:43:12 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask, request, Response
 from io import BytesIO
 import requests
@@ -37,6 +38,50 @@ def convert_pdf_to_images():
 
     response_data = {'images': images}
     return Response(json.dumps(response_data), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT, debug=True)
+'''
+from flask import Flask, jsonify, request
+import requests
+import base64
+import tempfile
+import os
+from pdf2image import convert_from_bytes
+from io import BytesIO
+from PIL import Image
+
+app = Flask(__name__)
+PORT = 3000
+HOST = '0.0.0.0'
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/convert')
+def convert():
+    document_url = request.args.get('url')
+    if not document_url:
+        return jsonify({'error': 'URL is required'})
+
+    try:
+        response = requests.get(document_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)})
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        images = []
+        pages = convert_from_bytes(response.content, output_folder=temp_dir)
+        for page in pages:
+            img_buffer = BytesIO()
+            page.save(img_buffer, format='PNG')
+            img_buffer.seek(0)
+            img_data = base64.b64encode(img_buffer.read()).decode('utf-8')
+            images.append(img_data)
+
+    return jsonify({'images': images})
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
