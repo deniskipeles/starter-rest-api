719809923109d669df35bfcb874a5fd26fb96785
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-05-07 22:13:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -30,68 +30,6 @@ from docx2pdf import convert as docx2pdf
 CHUNK_SIZE = 3
 OUTPUT_FORMAT = "png"
 
-
-
-@app.route('/convert')
-def convert():
-    document_url = request.args.get('url')
-    page_number = request.args.get('pages') or 1
-    page_number = int(page_number)
-    if not document_url:
-        return jsonify({'error': 'URL is required'})
-    # Get the file type from - response header
-    response = None
-    images = []
-    temp_image = None
-    input_file = None
-    try:
-        # Download the document file to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            response = requests.get(document_url, stream=True)
-            response.raise_for_status()
-            for chunk in response.iter_content(chunk_size=8192):
-                temp_file.write(chunk)
-        input_file = temp_file.name
-
-        # Check the file type and split the file into chunks if necessary
-        file_ext = mimetypes.guess_extension(mimetypes.guess_type(input_file)[0])
-        if file_ext:
-            file_ext = file_ext
-        else:
-            return jsonify({'error': 'Unsupported file type'})
-
-        if file_ext == '.docx':
-            input_file = docx2pdf(input_file)
-            file_ext = '.pdf'
-
-        if file_ext == '.pdf':
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.jpeg') as temp_image:
-                doc = fitz.open(input_file)
-                pages = min(page_number, doc.page_count)
-                for i in range(pages):
-                    page = doc[i]
-                    pix = page.get_pixmap()
-                    pix.save(temp_image.name)
-                    with open(temp_image.name, 'rb') as f:
-                        img_data = base64.b64encode(f.read()).decode('utf-8')
-                        images.append(img_data)
-                doc.close()
-        else:
-            return jsonify({'error': 'Unsupported file type'})
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-    finally:
-        # Delete the temporary input and image files
-        if input_file and os.path.exists(input_file):
-            os.remove(input_file)
-        if temp_image and os.path.exists(temp_image.name):
-            os.remove(temp_image.name)
-
-    return jsonify({'images': len(images)})
-
-'''
 @app.route('/convert')
 def convert():
     document_url = request.args.get('url')
@@ -110,10 +48,10 @@ def convert():
         content_type = response.headers.get('content-type')
         mime_type, _ = mimetypes.guess_type(document_url)
         file_ext = mimetypes.guess_extension(mime_type)
-        #file_ext = mimetypes.guess_extension(content_type)
-        if not file_ext:
+        extension = mimetypes.guess_extension(content_type)
+        if not file_ext and not extension:
             return jsonify({'error': 'Unknown file type'})
-        if file_ext == '.docx':
+        if file_ext == '.docx' or extension == '.docx':
             # Convert DOCX file to PDF
             with tempfile.NamedTemporaryFile(delete=False, suffix='.docx') as temp_docx:
               for chunk in response.iter_content(chunk_size=8192):
@@ -158,7 +96,7 @@ def convert():
 
     return jsonify({'images': len(images)})
 
-
+'''
 @app.route('/convert')
 def convert():
     document_url = request.args.get('url')
