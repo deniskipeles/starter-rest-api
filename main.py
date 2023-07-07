a38e3f97eeba9ab18768cfccfe99e4e036fc6cc3
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-07-07 16:22:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,161 +1,38 @@
-import os
-import re
-import string
-import random
-import json
+from flask import Flask, request, jsonify
 import requests
-# from bardapi.constants import SESSION_HEADERS
-SESSION_HEADERS = {
-    "Host": "bard.google.com",
-    "X-Same-Domain": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    "Origin": "https://bard.google.com",
-    "Referer": "https://bard.google.com/",
-}
 
-class Bard:
-    """
-    Bard class for interacting with the Bard API.
-    """
+app = Flask(__name__)
 
-    def __init__(
-        self,
-        token: str = None,
-        timeout: int = 20,
-        proxies: dict = None,
-        session: requests.Session = None,
-    ):
-        """
-        Initialize the Bard instance.
 
-        Args:
-            token (str): Bard API token.
-            timeout (int): Request timeout in seconds.
-            proxies (dict): Proxy configuration for requests.
-            session (requests.Session): Requests session object.
-        """
-        self.token = token or os.getenv("_BARD_API_KEY")
-        self.proxies = proxies
-        self.timeout = timeout
-        self._reqid = int("".join(random.choices(string.digits, k=4)))
-        self.conversation_id = ""
-        self.response_id = ""
-        self.choice_id = ""
-        # Set session or Get session
-        if session is None:
-            self.session = requests.Session()
-            self.session.headers = SESSION_HEADERS
-            self.session.cookies.set("__Secure-1PSID", self.token)
-        else:
-            self.session = session
-        self.SNlM0e = self._get_snim0e()
 
-    def get_answer(self, input_text: str) -> dict:
-        """
-        Get an answer from the Bard API for the given input text.
+from bard import Bard
+timeout = 30  # Increased timeout in seconds
 
-        Example:
-        >>> token = 'xxxxxxxxxx'
-        >>> bard = Bard(token=token)
-        >>> response = bard.get_answer("나와 내 동년배들이 좋아하는 뉴진스에 대해서 알려줘")
-        >>> print(response['content'])
+@app.route('/api/ai', methods=['POST'])
+def handle_post():
+    data = request.get_json()
 
-        Args:
-            input_text (str): Input text for the query.
-
-        Returns:
-            dict: Answer from the Bard API in the following format:
-                {
-                    "content": str,
-                    "conversation_id": str,
-                    "response_id": str,
-                    "factualityQueries": list,
-                    "textQuery": str,
-                    "choices": list,
-                    "links": list,
-                    "imgaes": set,
-                    "code": str
-                }
-        """
-        params = {
-            "bl": "boq_assistant-bard-web-server_20230419.00_p1",
-            "_reqid": str(self._reqid),
-            "rt": "c",
-        }
-       
-        # Make post data structure and insert prompt
-        input_text_struct = [
-            [input_text],
-            None,
-            [self.conversation_id, self.response_id, self.choice_id],
-        ]
-        data = {
-            "f.req": json.dumps([None, json.dumps(input_text_struct)]),
-            "at": self.SNlM0e,
-        }
-
-        # Get response
-        resp = self.session.post(
-            "https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate",
-            params=params,
-            data=data,
-            timeout=self.timeout,
-            proxies=self.proxies,
-        )
-
-        # Post-processing of response
-        resp_dict = json.loads(resp.content.splitlines()[3])[0][2]
-
-        if not resp_dict:
-            return {"content": f"Response Error: {resp.content}. \nTemporarily unavailable due to traffic or an error in cookie values. Please double-check the cookie values and verify your network environment."}
-        resp_json = json.loads(resp_dict)
-
-        # Parsed Answer Object
-        parsed_answer = json.loads(resp_dict)
-
-        # Returnd dictionary object
-        bard_answer = {
-            "content": parsed_answer[4][0][1][0],
-            "conversation_id": parsed_answer[1][0],
-            "response_id": parsed_answer[1][1],
-            "factualityQueries": parsed_answer[3],
-            "textQuery": parsed_answer[2][0] if parsed_answer[2] else "",
-            "choices": [{"id": x[0], "content": x[1]} for x in parsed_answer[4]],
-        }
-        self.conversation_id, self.response_id, self.choice_id = (
-            bard_answer["conversation_id"],
-            bard_answer["response_id"],
-            bard_answer["choices"][0]["id"],
-        )
-        self._reqid += 100000
-
-        return bard_answer
     
+    if data:
+        rules = data.get('rules', '.')
+        
+        bard = Bard(token="YAjim5M18K27kXedkuWI_6phq1LXgU-1u3gfLDbpgfubpIhn1AhN-OYdWhmNaRIwnTAChg.")
+        res = bard.get_answer('Question:>>>>>'+data['question']+"<<<<<. Rules:>>>>> "+rules+'<<<<<')
+        print(res)
 
-    def _get_snim0e(self) -> str:
-        """
-        Get the SNlM0e value from the Bard API response.
+        answer = res['content']
+        links = res['links'][:10]
+        images = list(res['images'])[:10]
 
-        Returns:
-            str: SNlM0e value.
-        Raises:
-            Exception: If the __Secure-1PSID value is invalid or SNlM0e value is not found in the response.
-        """
-        if not self.token or self.token[-1] != ".":
-            raise Exception(
-                "__Secure-1PSID value must end with a single dot. Enter correct __Secure-1PSID value."
-            )
-        resp = self.session.get(
-            "https://bard.google.com/", timeout=self.timeout, proxies=self.proxies
-        )
-        if resp.status_code != 200:
-            raise Exception(
-                f"Response code not 200. Response Status is {resp.status_code}"
-            )
-        snim0e = re.search(r"SNlM0e\":\"(.*?)\"", resp.text)
-        if not snim0e:
-            raise Exception(
-                "SNlM0e value not found. Double-check __Secure-1PSID value or pass it as token='xxxxx'."
-            )
-        return snim0e.group(1)
+        response_data = {
+            'answer': answer,
+            'links': links,
+            'images': images
+        }
+
+        return jsonify(response_data)
+
+    return jsonify({'error': 'No external data available'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
