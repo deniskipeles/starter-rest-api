ccc2f8cba9d6d783ee8938bfa0879b82e7f7943b
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-07-07 16:14:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -264,6 +264,37 @@ def pdf_to_html():
 
 
 
+from bard import Bard
+timeout = 30  # Increased timeout in seconds
+
+@app.route('/api/ai', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+
+    
+    if data:
+        rules = data.get('rules', '.')
+        
+        bard = Bard(token="YAjim5M18K27kXedkuWI_6phq1LXgU-1u3gfLDbpgfubpIhn1AhN-OYdWhmNaRIwnTAChg.")
+        res = bard.get_answer('Question:>>>>>'+data['question']+"<<<<<. Rules:>>>>> "+rules+'<<<<<')
+        print(res)
+
+        answer = res['content']
+        links = res['links'][:10]
+        images = list(res['images'])[:10]
+
+        response_data = {
+            'answer': answer,
+            'links': links,
+            'images': images
+        }
+
+        return jsonify(response_data)
+
+    return jsonify({'error': 'No external data available'})
+
+
+
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
 
